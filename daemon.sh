0ad78f6f8521ed6f0e1ee888813126b1b8a36f1a
Joe Baker <joesbaker@gmail.com>
2014-08-21 10:28:49 +0100

    First commit - dashing stuff

---
+++ daemon.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+DESC="Dashing"
+DASHING_DIR="/var/www/dashing"
+INSTANCE_DIR="$DASHING_DIR/wunderroot_dashboard"
+PIDFILE="$INSTANCE_DIR/tmp/pids/unicorn.pid"
+DAEMON="unicorn"
+DAEMONOPTS=" -c ./unicorn.rb -p 3030 -D"
+STOPOPTS="stop"
+#GEM_HOME=/usr/local/rvm/gems/ruby-2.1.2
+
+#store dashing pid in $PID, else empty string
+if [ -f $PIDFILE ]; then
+  PID=$(<"$PIDFILE")
+#else
+#  echo ""
+fi
+
+case "$1" in
+start)
+    printf "%-50s" "Starting $DESC..."
+    cd $INSTANCE_DIR ; $DAEMON $DAEMONOPTS
+    if [ -f $PIDFILE ]; then
+        printf "%s\n" "Ok"
+    else
+        printf "%s\n" "Fail"
+    fi
+    ;;
+stop)
+    echo "Killing $DESC..."
+    if [ -f $PIDFILE ]; then
+#      cd $INSTANCE_DIR ; $DAEMON $STOPOPTS
+#      sleep 1s && echo "RIP $DESC"
+       cat $PIDFILE | xargs kill -QUIT && sleep 3s && echo "RIP $DESC"
+    else
+      printf "Stop beating the dead horse (it's already dead!)\n"
+    fi
+    ;;
+restart)
+    $0 stop
+    $0 start
+    ;;
+logs)
+    echo "See the logs of the Dashing."
+    tail -f `$INSTANCE_DIR/log/unicorn.log`
+    ;;
+status)
+    # Check to see if the process is running
+    ps aux | grep -i dashing
+    ;;
+  esac
+  exit
---
+++ unicorn.rb
@@ -0,0 +1,26 @@
+# Set the working application directory
+# # working_directory "/path/to/your/app"
+@dir = "/var/www/dashing/wunderroot_dashboard/"
+
+working_directory @dir
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/logs/unicorn.log"
+# stdout_path "/path/to/logs/unicorn.log"
+stderr_path "#{@dir}log/unicorn.log"
+stdout_path "#{@dir}log/unicorn.log"
+
+# Unicorn socket
+# listen "/tmp/unicorn.[app name].sock"
+listen "/tmp/unicorn.wrdashing.sock", :backlog => 64
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
